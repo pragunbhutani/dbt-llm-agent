f6e9f3c945e13d55a610688a9648fcd4a687f587
PRAGUN_BHUTANI <1109752+pragunbhutani@users.noreply.github.com>
2025-04-01 08:14:17 +0530

    bumped project version and updated readme

--- dbt_llm_agent/commands/version_cmd.py
+++ dbt_llm_agent/commands/version_cmd.py
@@ -9,4 +9,4 @@ from dbt_llm_agent.utils.cli_utils import colored_echo
 @click.command()
 def version():
     """Get the version of dbt-llm-agent"""
-    colored_echo("dbt-llm-agent version 0.1.0", color="INFO", bold=True)
+    colored_echo("dbt-llm-agent version 0.4.0", color="INFO", bold=True)
