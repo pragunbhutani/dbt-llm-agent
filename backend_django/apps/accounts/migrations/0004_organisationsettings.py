4b8d52e7d2ed1aed232ab65473d0e2d0c779aa2d
PRAGUN_BHUTANI <1109752+pragunbhutani@users.noreply.github.com>
2025-06-24 10:40:43 +0530

    fixed migrations

--- backend_django/apps/accounts/migrations/0004_organisationsettings.py
+++ backend_django/apps/accounts/migrations/0004_organisationsettings.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("accounts", "0003_alter_user_email"),
+        ("accounts", "0001_initial"),
     ]
 
     operations = [
