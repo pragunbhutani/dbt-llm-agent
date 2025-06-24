273bac3c5ec41bfbae6bf56534b99322b145ac21
PRAGUN_BHUTANI <1109752+pragunbhutani@users.noreply.github.com>
2025-06-24 10:51:20 +0530

    fixed migration deps again

--- backend_django/apps/embeddings/migrations/0002_modelembedding_organisation.py
+++ backend_django/apps/embeddings/migrations/0002_modelembedding_organisation.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("accounts", "0002_initial"),
+        ("accounts", "0001_initial"),
         ("embeddings", "0001_initial"),
     ]
 
